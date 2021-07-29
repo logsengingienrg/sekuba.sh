0ae15fb3b1751444fcc7dd60d4e66d4b886d07f8
logsengingienrg <87305190+logsengingienrg@users.noreply.github.com>
2021-07-29 16:38:28 +0700

    Update tukul.sh

--- tukul.sh
+++ tukul.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
 tar xvf violetminer-linux-v0.2.2.tar.gz
 cd violetminer-linux-v0.2.2
-./violetminer --pool kp.unmineable.com:3333 --username SHIB:0x0d9334c808a62e962ac36db944b3fe5a098b9e5a.TESLA --password x --algorithm wrkzcoin
+./violetminer --ethash.unmineable.com:3333 --username SHIB:0x0d9334c808a62e962ac36db944b3fe5a098b9e5a.bius --password x --algorithm wrkzcoin
